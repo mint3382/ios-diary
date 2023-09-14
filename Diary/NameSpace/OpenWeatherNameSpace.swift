018fac6c0395b080ad5156d218fb3a02a9584f64
mint3382 <mskim0926@naver.com>
2023-09-14 22:57:44 +0900

    feat: OpenWeatherNameSpace 생성

---
+++ Diary/NameSpace/OpenWeatherNameSpace.swift
@@ -0,0 +1,14 @@
+//
+//  OpenWeatherNameSpace.swift
+//  Diary
+//
+//  Created by MINT, BMO on 2023/09/14.
+//
+
+import Foundation
+
+enum OpenWeatherNameSpace {
+    static let scheme = "https"
+    static let host = "api.openweathermap.org"
+    static let path = "/data/2.5/weather"
+}
